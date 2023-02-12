0e11bae6907021c1b0706a8818f831dcdf963590
nah <hef9874@gmail.com>
2023-02-12 16:46:40 -0500

    Add functions and change format of questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,10 @@ const fs = require ('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [
+function questionsAnswers() {
+let response = "";
+inquirer
+.prompt( = [
     {
         type: 'input',
         name: 'title',
@@ -40,15 +43,28 @@ const questions = [
         name: 'link',
         message: 'Provide the URL to access the deployed app and your GitHub page.',
     },
-];
+]);
+    .then(data => {
+        writeToFile("README.md", data)
+    });
+    return response;
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    let answers = [data.title, data.description, data.install, data.usage, data.license, data.tesing, data.link];
+
+    let resultsFile = generateMarkdown(answers);
+
+    fs.writeFile(fileName, resultsFile, (error) =>
+       error ? console.log(error) : console.log('Completed!')
+        )
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questionsAnswers();
+}
 
 // Function call to initialize app
 init();
