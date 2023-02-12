f84b9c628b029886f248a15111ca054c34ad2fe7
nah <hef9874@gmail.com>
2023-02-12 16:28:32 -0500

    Add questions and input types to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the name of your project.',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the purpose of your project.',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation steps.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide the path to a screenshot of your application and usage instructions',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license used on this project.',
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Enter testing instructions.',
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Provide the URL to access the deployed app and your GitHub page.',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
